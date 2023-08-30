e37daa41b14c458efe7883128041a837fecf1124
PallaviKatari <pallavikatari@gmail.com>
2023-08-30 14:19:47 +0530

    commit_30.08.2023

--- Role_Menu_MVC/UsersModel.Context.cs
+++ Role_Menu_MVC/UsersModel.Context.cs
@@ -31,22 +31,22 @@ namespace Role_Menu_MVC
         public virtual DbSet<UserActivation> UserActivations { get; set; }
         public virtual DbSet<Role> Roles { get; set; }
     
-        public virtual ObjectResult<Nullable<int>> Insert_User(string username, string password, string email)
-        {
-            var usernameParameter = username != null ?
-                new ObjectParameter("Username", username) :
-                new ObjectParameter("Username", typeof(string));
+        //public virtual ObjectResult<Nullable<int>> Insert_User(string username, string password, string email)
+        //{
+        //    var usernameParameter = username != null ?
+        //        new ObjectParameter("Username", username) :
+        //        new ObjectParameter("Username", typeof(string));
     
-            var passwordParameter = password != null ?
-                new ObjectParameter("Password", password) :
-                new ObjectParameter("Password", typeof(string));
+        //    var passwordParameter = password != null ?
+        //        new ObjectParameter("Password", password) :
+        //        new ObjectParameter("Password", typeof(string));
     
-            var emailParameter = email != null ?
-                new ObjectParameter("Email", email) :
-                new ObjectParameter("Email", typeof(string));
+        //    var emailParameter = email != null ?
+        //        new ObjectParameter("Email", email) :
+        //        new ObjectParameter("Email", typeof(string));
     
-            return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<Nullable<int>>("Insert_User", usernameParameter, passwordParameter, emailParameter);
-        }
+        //    return ((IObjectContextAdapter)this).ObjectContext.ExecuteFunction<Nullable<int>>("Insert_User", usernameParameter, passwordParameter, emailParameter);
+        //}
     
         public virtual ObjectResult<RoleUser> ValidateUser(string username, string password)
         {
